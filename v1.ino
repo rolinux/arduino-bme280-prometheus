d1856cc69010c526ee01e8dc8894599b0c8ce365
rolinux <rolinux@users.noreply.github.com>
2019-06-20 21:20:09 +0100

    Simplify the delay

--- v1.ino
+++ v1.ino
@@ -1,5 +1,5 @@
-// ESP8266 WebServer - using DHT11 sensor
-// (c) D L Bird 2016
+// ESP8266 WebServer - using BME280 sensor
+// https://github.com/rolinux/arduino-bme280-prometheus
 //
 #include <Wire.h>
 #include <ESP8266WiFi.h>
@@ -9,42 +9,20 @@
 #include "prometheus.h"
 #include "config.h"
 
-#define MAX_LOOP_TIME_MS     30000
-
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 const String metrics_url = "/metrics/job/" + String(METRICS_JOB) +
                            "/instance/" + String(METRICS_INSTANCE);
 
-Ticker sleepTicker;
-
 Adafruit_BME280 bme; // Note Adafruit assumes I2C adress = 0x77 my module (eBay) uses 0x76 so the library address has been changed.
 
-unsigned long startTime;
-
-void sleepyTime() {
-  const int elapsed = millis() - startTime;
-  Serial.printf("Sleeping. Loop took %d ms\n", elapsed);
-  // If this sleep happened because of timeout, clear the
-  // Wifi state.
-  if (elapsed >= MAX_LOOP_TIME_MS) {
-    WiFi.disconnect();
-  }
-  ESP.deepSleep(480000000, WAKE_RF_DEFAULT);
-  // It can take a while for the ESP to actually go to sleep.
-  // When it wakes up we start again in setup().
-  delay(5000);
-}
-
 void setup() {
-  startTime = millis();
-  sleepTicker.once_ms(12000, &sleepyTime);
   Serial.begin(115200);
   Serial.print("Connecting to ");
   Serial.println(ssid);          // Connect to WiFi network
   WiFi.config(ip, gateway, subnet);
   WiFi.persistent(false);  // disables the storage of credentials to flash.
-  WiFi.begin(ssid, password);   
+  WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -52,7 +30,7 @@ void setup() {
   Serial.println("WiFi connected..");
   pinMode(D3, INPUT_PULLUP); //Set input (SDA) pull-up resistor on
 
-  Wire.setClock(2000000);    // Set I2C bus speed 
+  Wire.setClock(2000000);    // Set I2C bus speed
   Wire.begin(D2,D1); // Define which ESP8266 pins to use for SDA, SCL of the Sensor
   if (!bme.begin(0x76)) {
     Serial.println("Could not find a valid BME280 sensor, check wiring!");
@@ -80,7 +58,7 @@ void loop(void)
   PrometheusClient pclient =
       PrometheusClient(SERVER_IP, SERVER_PORT,
                        METRICS_JOB, METRICS_INSTANCE);
-                           
+
   float temp = bme.readTemperature();
   float humidity = bme.readHumidity();
   while (!(isValidTemp(temp) && isValidHumidity(humidity))) {
@@ -89,18 +67,11 @@ void loop(void)
     humidity = bme.readHumidity();
   }
 
-  const int bmeTime = millis() - startTime;
-  Serial.printf("BME read took %d ms\n", bmeTime);
-  // waitForWifi();
-  const int wifiTime = millis() - (startTime + bmeTime);
-  Serial.printf("WiFi init took an additional %d ms\n", wifiTime);
- 
   pclient.AddMetric(makeMetric("humidity", humidity));
   pclient.AddMetric(makeMetric("temperature", temp));
   pclient.AddMetric(makeMetric("free_heap", ESP.getFreeHeap()));
   pclient.AddMetric(makeMetric("pressure", bme.readPressure()));
-  pclient.AddMetric(makeMetric("loop_time", float(millis() - startTime)));
   pclient.PrintSerial();
   pclient.Send();
-  sleepyTime();
+  delay(30000);        // Control speed of BME280 sensor reading
 }
