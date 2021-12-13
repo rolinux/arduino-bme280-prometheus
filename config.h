4ed02a7273fe40be2bbf07923714b715db9d73a7
Radu Coroi <rolinux@users.noreply.github.com>
2021-12-13 22:22:18 +0000

    Add second push gateway server

--- config.h
+++ config.h
@@ -5,11 +5,12 @@ const char *password = "*********"; // Your password here
 #define METRICS_JOB         "env_sensor"
 #define METRICS_INSTANCE    "Office" // where the Arduino is located
 
-#define SERVER_IP           "192.168.7.10" // target Prometheus Pushgateway IP
+#define SERVER_IP_1        "192.168.64.2" // target Prometheus Pushgateway IP - 1st server
+#define SERVER_IP_2        "192.168.64.3" // target Prometheus Pushgateway IP - 2nd server
 #define SERVER_PORT         9091  // target Prometheus Pushgateway port
 
 #define DELAY_BETWEEN_READINGS         30000  // in miliseconds
 
-IPAddress ip(192, 168, 252, 20);   // The address 192.168.252.20 is in my case on the IoT VLAN
-IPAddress gateway(192,168,252,1);  // My router has this base address
-IPAddress subnet(255,255,254,0); // Define the sub-network (/23)
+IPAddress ip(192, 168, 64, 22);  // Office IP for the Arduino
+IPAddress gateway(192, 168, 64, 1); // My router has this base address
+IPAddress subnet(255, 255, 224, 0); // Define the sub-network
